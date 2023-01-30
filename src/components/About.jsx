561dd95e6e0c15b1bd0bd093eceb850db85b5b93
Gary Shaw <garyshaw05@gmail.com>
2023-01-30 16:45:40 +0000

    updated projects section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import imageMe from "./assets/images/headshot.png";
+import imageMe from "./assets/images/headshot.jpeg";
 import "../App.css";
 
 const About = () => {
@@ -7,16 +7,45 @@ const About = () => {
     <div className="content">
       <div className="even-columns">
         <div className="col">
-          <p>
-            Hi. My name is Gary Shaw
-            <br />
-            This is my portfolio website
-            <br />
-            Feel free to look around 🙂
-          </p>
+          <img src={imageMe} alt="Gary Shaw" className="headshot" />
         </div>
         <div className="col">
-          <img src={imageMe} alt="Gary Shaw" className="headshot" />
+          <div className="col-text-left">
+            <p>
+              Hi. I'm Gary Shaw.
+              <br />
+              <br />I love all things software, gadgets, dogs 🐶 and my family
+              🥰
+            </p>
+            <br />
+            <p>
+              This is my portfolio website where I'll be adding the projects I
+              have worked on so far. It's a work in progress but feel free to
+              look around 🙂
+              <br />
+              <br />
+              I completed the Northcoders full-stack software engineering
+              bootcamp in January 2023 and prior to that I worked with
+              enterprise software in pre-sales and post-sales.
+              <br />
+              <br />
+              That has given me great experience across the whole software
+              lifecycle from design & development through to sales,
+              implementation & support.
+              <br />
+              <br />
+              I'm looking for a role where I can combine my love of working with
+              software and also helping people to use and benefit from it
+              themselves.
+              <br />
+              <br />
+              Feel free to get in touch using the{" "}
+              <a href="#links" className="link">
+                links
+              </a>{" "}
+              below.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,32 +6,38 @@ const Skills = () => {
       {/* <h2 className="sectionTitle">Skills</h2> */}
       <div className="even-columns">
         <div className="col">
-          <h3>front end</h3>
-          <ul className="skillList">
-            <li className="skillListItem">React</li>
-            <li className="skillListItem">React Native</li>
-            <li className="skillListItem">HTML5</li>
-            <li className="skillListItem">CSS</li>
-          </ul>
+          <div className="col-box">
+            <h3 className="col-title">Front end</h3>
+            <ol className="skillList">
+              <li className="skillListItem">React</li>
+              <li className="skillListItem">React Native</li>
+              <li className="skillListItem">HTML5</li>
+              <li className="skillListItem">CSS</li>
+            </ol>
+          </div>
         </div>
         <div className="col">
-          <h3>back end</h3>
-          <ul className="skillList">
-            <li className="skillListItem">node.js</li>
-            <li className="skillListItem">PostgreSQL</li>
-            <li className="skillListItem">MongoDB</li>
-            <li className="skillListItem">Express</li>
-          </ul>
+          <div className="col-box">
+            <h3 className="col-title">Back end</h3>
+            <ul className="skillList">
+              <li className="skillListItem">node.js</li>
+              <li className="skillListItem">PostgreSQL</li>
+              <li className="skillListItem">MongoDB</li>
+              <li className="skillListItem">Express</li>
+            </ul>
+          </div>
         </div>
 
         <div className="col">
-          <h3>fundamentals</h3>
-          <ul className="skillList">
-            <li className="skillListItem">test driven development</li>
-            <li className="skillListItem">Git version control</li>
-            <li className="skillListItem">pair programming</li>
-            <li className="skillListItem">object-oriented programming</li>
-          </ul>
+          <div className="col-box">
+            <h3 className="col-title">Fundamentals</h3>
+            <ul className="skillList">
+              <li className="skillListItem">test driven development</li>
+              <li className="skillListItem">Git version control</li>
+              <li className="skillListItem">pair programming</li>
+              <li className="skillListItem">object-oriented programming</li>
+            </ul>
+          </div>
         </div>
         {/* <div class="col"></div>
         <div class="col"></div> */}
